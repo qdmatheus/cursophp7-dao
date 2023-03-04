c91f7d968d30d33a63965a1c4996a29e3c19f6a6
Matheus Quadros <useryou@Users-MacBook-Pro.local>
2023-03-04 15:48:06 -0300

    ajustes parte 2

--- index.php
+++ index.php
@@ -3,15 +3,25 @@
     require_once("config.php");
 
     /*$sql = new Sql();
-
     $usuarios = $sql -> select("SELECT * FROM tb_usuarios");
-
     echo json_encode($usuarios);*/
 
+    /*Carrega um usuario do loadById
     $root = new Usuario();
-
     $root -> loadById(3);
+    echo $root;*/
 
-    echo $root;
+    /*Carrega a lista de usuarios
+    $lista = Usuario::getList();
+    echo json_encode($lista);*/
+
+    /*Carrega uma lista de usuarios buscando pelo locale_get_display_name
+    $search = Usuario::search("us");
+    echo json_encode($search);*/
+
+    //Carrega um usuario usando o login e a senha
+    $usuario = new Usuario();
+    $usuario -> login("user", "666");
+    echo $usuario;
 
 ?>
